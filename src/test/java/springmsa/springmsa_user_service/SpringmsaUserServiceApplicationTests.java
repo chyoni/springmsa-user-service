6d5c80ff1d2454860a7bf2a0ae8020764f95ef1d
chyonee <chiwon99881@gmail.com>
2024-01-04 16:45:55 +0900

    User Service init

---
+++ src/test/java/springmsa/springmsa_user_service/SpringmsaUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package springmsa.springmsa_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmsaUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
